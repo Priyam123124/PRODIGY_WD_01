d5b1d008c874f6c6e36f176149956400ed7268df
Priyam <priyamber0684@gmail.com>
2024-02-06 12:13:18 +0530

    Final Version

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let dropdown = document.getElementById('box');
 let element = document.getElementById('dropdown');
 let switch1 = document.getElementById('flexSwitchCheckDefault');
 let id1 = document.getElementById('container1');
+let id2 = document.getElementById('background');
 let nav = document.getElementById('navbar1');
 let image = document.getElementById('immg');
 let check = false;
@@ -19,18 +20,20 @@ dropdown.addEventListener("click", ()=>{
 switch1.addEventListener("click", ()=>{
     check2 = !check2
     if(check2){
-        console.log("Enabled");
+        document.body.style.backgroundColor = "rgb(35, 34, 34)";
         id1.style.backgroundColor = "rgb(35, 34, 34)";
+        id2.style.backgroundColor = "rgb(35, 34, 34)";
         nav.style.backgroundColor = "black";
         id1.style.color = "white";
+        id2.style.color = "white";
         image.innerHTML = '<img src="dark.jpg" class="image" alt="Banner">';
     } else {
+        document.body.style.backgroundColor = "azure";
         id1.style.backgroundColor = "azure";
+        id2.style.backgroundColor = "azure";
         id1.style.color = "black";
+        id2.style.color = "black";
         nav.style.backgroundColor = "#23413E";
         image.innerHTML = '<img src="banner.jpg" class="image" alt="Banner">';
-        console.log("Disabled");
     }
 })
-
-document.body.style.backgroundColor = "black"
